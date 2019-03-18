08e2eb05bfbd524a842a27b9e6610edac5626972
Felipe Sena <feelipe.27@gmail.com>
2019-03-18 12:59:04 -0300

    Create Problem1180.java

---
+++ ProjectsUri/src/br/edu/univas/main/Problem1180.java
@@ -0,0 +1,33 @@
+package br.edu.univas.main;
+
+import java.util.Scanner;
+
+public class Problem1180 {
+
+	public static void main(String[] args) {
+	       int n, temp, lower = 99, cont = 0;
+			Scanner input = new Scanner(System.in);
+			n = input.nextInt();
+			int vet[] = new int[n];
+			boolean firstLoop = false;
+			
+			for(int i = 0; i < n; i++) {
+				vet[i] = input.nextInt(); // inicializacao do array
+			}
+			
+			for(int j = 0; j < n; j++) { // verifica o menor
+				 temp = vet[j];
+		            if (firstLoop == false) {
+		                lower = temp;
+		                firstLoop = true;
+		            } if (temp < lower) {
+		                 lower = temp;
+		                 cont = j;
+		             }
+				
+			}
+			System.out.println("Menor valor: " + lower);
+			System.out.println("Posicao: " + cont);
+	}
+
+}
